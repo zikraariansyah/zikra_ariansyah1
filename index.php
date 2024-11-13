4cacbbad37123306609eee7fba1a743b0a7dd0da
zikraariansyah <zikrasbg299@gmail.com>
2024-11-13 14:50:27 +0700

    membuat file index

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project is62</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<?php
+include_once('navbar.php');
+?>
+
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+        <div class="card">
+            <div class="card-header">
+                <h3>Sistem Informasi Mahasiswa</h3>
+            </div>
+            <div class="card-body">
+                <h4>Selamat Datang,Administrator</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto molestiae quae voluptate dolore nisi repellendus voluptas numquam! Consequatur, sit minima atque eveniet, beatae excepturi ipsa perspiciatis at soluta culpa et?</p>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
